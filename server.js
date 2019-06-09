7c507611887fc4f93b5984ca9b27e49a10880bf1
Anadelaine Catalla <anadelaine.catalla@gmail.com>
2019-06-08 21:47:08 -0700

    set up port, express app to handle data parsing and added listener

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 const express = require("express");
 const path = require("path");
 
-// sets up express app and port to 8080
+// sets up express app and port to 3000
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
+// sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// starts the server
+app.listen(port, function(){
+    console.log("App listening on PORT: " + port)
+})
 // 10 questions and a scale 1 to 5 based on how much the user agrees or disgrees 
 
