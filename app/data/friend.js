b239aed36801df00bff8eb3d3317d4bc67834062
Anadelaine Catalla <anadelaine.catalla@gmail.com>
2019-06-12 19:09:19 -0700

    added photos and added get in apiRoute

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,53 +1,53 @@
 // holds friends data in an an array of ojbects with name, photo and score
 var friends = [
     {
-        name: "Zoe",
-        photo: "",
+        name: "Zoe Kravitz",
+        photo: "https://www.telegraph.co.uk/content/dam/beauty/2017/11/16/ZOE-KRAVITZ-02-ELODIE-DAGUIN_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpg?imwidth=450",
         scores:[5, 4, 3, 2, 1, 2, 3, 4, 5, 3]
     },
     {
-        name: "Trevor",
-        photo: "",
+        name: "Klay Thompson",
+        photo: "https://s.hdnux.com/photos/01/02/56/64/17493273/3/920x920.jpg",
         scores:[1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
     }, 
     {
-        name: "Gina",
-        photo: "",
+        name: "Jorja Smith",
+        photo: "https://indie-mag.com/wp-content/uploads/2018/09/010_081_v2_RGB.jpg",
         scores: [2, 4, 5, 3, 1, 5, 3, 2 , 1, 3]
     },
     {
-        name: "Stephen",
-        photo: "",
+        name: "Stephen Curry",
+        photo: "https://www.magnificentbastard.com/images/pics/steph-curry-toolbag.jpg",
         scores: [3, 5, 1, 2, 3, 4, 4, 2, 5, 2]
     },
     {
-        name: "Tina",
-        photo: "",
+        name: "Jhene Aiko",
+        photo: "https://i0.wp.com/theblast.com/wp-content/uploads/2018/08/jhen_main.jpg?fit=1024%2C768&ssl=1",
         scores: [4, 2, 5, 2, 2, 5, 1, 1, 3, 5]
     },
     {
-        name: "Martin",
-        photo: "",
+        name: "Martin Lawrence",
+        photo: "https://i.pinimg.com/originals/94/06/46/940646477e67949a6479e929a210920c.jpg",
         scores: [3, 2, 1, 5, 4, 3, 2, 4, 2, 4]
     },
     {
-        name: "Raf",
-        photo: "",
+        name: "Jessica Alba",
+        photo: "https://media.self.com/photos/57d8990df71ce8751f6b4e90/master/pass/jessica-alba-blue-skirt.jpg",
         scores: [4, 2, 3, 3, 2, 1, 4, 2, 5, 5]
     },
     {
-        name: "Don",
-        photo: "",
+        name: "Asap Rocky",
+        photo: "https://www.billboard.com/files/styles/article_main_image/public/media/04-asap-rocky-portrait-billboard-1548.jpg",
         scores: [3, 2, 1, 5, 5, 3, 2, 1, 1, 3]
     },
     {
-        name: "Ana",
-        photo: "",
+        name: "Kehlani",
+        photo: "https://pbs.twimg.com/media/Dz_CKj4U0AAAgsd.jpg",
         scores: [4, 5, 2, 1, 5, 2, 4, 2, 5, 2]
     },
     {
-        name: "Rick",
-        photo: "",
+        name: "Travis Scott",
+        photo: "https://nyppagesix.files.wordpress.com/2019/05/travis-scott.jpg?quality=90&strip=all&w=618&h=410&crop=1",
         scores: [4, 1, 1, 4, 3, 5, 3, 2, 4, 2] 
     } 
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,13 @@
+// connect our object of friends
+var friends = require("../data/friend.js");
 // a GET route with the url /api/friends which displays a json of possible friends
 module.exports = function(app) {
-    app.post
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+    app.post("/api/friends", function(req, res){
+        
+    })
 }
 // a POST routes /api/friends which will be used to handle incoming survey results
 // this route also handles the compatibility logic
