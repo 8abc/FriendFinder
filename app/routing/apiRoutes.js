11e13dea00408e5c1730c413d7660ec097bef15a
8abc <anadelaine.catalla@gmail.com>
2019-06-18 22:54:40 -0700

    added modal and worked on logic to calculate sum for bestMatch

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,22 +7,41 @@ module.exports = function(app) {
     });
     // add a new friend
     app.post("/api/friends", function(req, res){
-        var userInput = req.body;
-        var userScores = userInput.scores;
         // holds the best match friend
         var bestMatch = {
             name: "",
             photo: "",
             totalDifference: 100,
         };
+        // holds results from user's survey
+        var userInput = req.body;
+        var userScores = userInput.scores;
         // used to calculate difference between the user's score and the scores of each friend in the database
         var totalDifference = 0;
         // loops through all existing friends
         for(var i =0; i<friends.length; i++) {
-            console.log(friends[i]);
+            var friend = friends[i];
             totalDifference = 0;
+            // loops through each friend's score
+        for (var x=0; i<friend.scores.length; x++) {
+            var friendScore = friend.scores[x];
+            var userScore = userScores[x];
+            // calculates the difference between the scores and sum them into totalDifference
+            totalDifference += Math.abs(parseInt(userScore)- parseInt(friendScore));
         }
-    })
-}
-// a POST routes /api/friends which will be used to handle incoming survey results
-// this route also handles the compatibility logic
+        // show if the difference is less then the differences of the current best match
+        if(totalDifference <= bestMatch.friendDifference) {
+            // reset bestMatch to be the new friend
+            bestMatch.name = friend.name;
+            bestMatch.photo = friend.photo;
+            bestMatch.friendDifference = totalDifference;
+        }
+        
+    }
+        // saves user's input into database
+        friends.push(userInput);
+    // returns a JSON with the user's bestMatch
+        res.json(bestMatch);
+
+    });
+};
