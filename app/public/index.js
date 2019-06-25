ed2e45215e10d1d78560aaa77087a145df10e6f7
8abc <anadelaine.catalla@gmail.com>
2019-06-24 19:38:45 -0700

    added chosen css and logic to survey

--- app/public/index.js
+++ app/public/index.js
@@ -1,31 +1,78 @@
-// var axios = require("axios");
-
-    $("#submit").on("click", function(){
-        event.preventDefault();
-            var userInput = {
-                name: $("#name").val().trim(),
-                photo: $("#photoLink").val().trim(),
-                scores: [
-                    $("#question1").val(),
-                    $("#question2").val(),
-                    $("#question3").val(),
-                    $("#question4").val(),
-                    $("#question5").val(),
-                    $("#question6").val(),
-                    $("#question7").val(),
-                    $("#question8").val(),
-                    $("#question9").val(),
-                    $("#question10").val(),
-                ]
-            };
-            console.log(userInput);
-
-            axios.post("/api/friends", userInput)
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function(error){
-                    console.log(error);
-                })
-            
-    });
+    // // Chosen CSS
+    // var config = {
+    //     ".chosen-select": {},
+    //     ".chosen-select-deselect": {
+    //       allow_single_deselect: true
+    //     },
+    //     ".chosen-select-no-single": {
+    //       disable_search_threshold: 10
+    //     },
+    //     ".chosen-select-no-results": {
+    //       no_results_text: "Oops, nothing found!"
+    //     },
+    //     ".chosen-select-width": {
+    //       width: "95%"
+    //     }
+    //   };
+  
+    //   for (var selector in config) {
+    //     $(selector).chosen(config[selector]);
+    //   }
+  
+    //   // Capture the form inputs
+    //   $("#submit").on("click", function(event) {
+    //     event.preventDefault();
+  
+    //     // Form validation
+    //     function validateForm() {
+    //       var isValid = true;
+    //       $(".form-control").each(function() {
+    //         if ($(this).val() === "") {
+    //           isValid = false;
+    //         }
+    //       });
+  
+    //       $(".chosen-select").each(function() {
+  
+    //         if ($(this).val() === "") {
+    //           isValid = false;
+    //         }
+    //       });
+    //       return isValid;
+    //     }
+  
+    //     // If all required fields are filled
+    //     if (validateForm()) {
+    //       // Create an object for the user"s data
+    //       var userInput = {
+    //         name: $("#name").val(),
+    //         photo: $("#photo").val(),
+    //         scores: [
+    //           $("#q1").val(),
+    //           $("#q2").val(),
+    //           $("#q3").val(),
+    //           $("#q4").val(),
+    //           $("#q5").val(),
+    //           $("#q6").val(),
+    //           $("#q7").val(),
+    //           $("#q8").val(),
+    //           $("#q9").val(),
+    //           $("#q10").val()
+    //         ]
+    //       };
+  
+    //       // AJAX post the data to the friends API.
+    //       $.post("/api/friends", userInput, function(data) {
+  
+    //         // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+    //         $("#matchName").text(data.name);
+    //         $("#matchPhoto").attr("src", data.photo);
+  
+    //         // Show the modal with the best match
+    //         $("#results-modal").modal("toggle");
+  
+    //       });
+    //     } else {
+    //       alert("Please fill out all fields before submitting!");
+    //     }
+    //   });
